490556f7bd4233863478823ad03ed8ce800dd6b0
Pranav Kulkarni <pranavkulkarni1024@gmail.com>
2023-01-02 12:22:24 -0500

    Building Calculator Logic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
+import { useReducer } from 'react';
 import './styles.css'
+
+const ACTIONS = {
+  ADD_DIGITS : 'add-digits',
+  CHOOSE_OPERATION : 'choose-operation',
+  CLEAR : 'clear',
+  DELETE_DIGIT : 'delete-digit',
+  EVALUATE : 'evaluate'
+}
+
+function reducer(state,{type,payload}){
+
+    switch(type){
+      case ACTIONS.ADD_DIGITS:
+
+      
+    }
+
+}
 
 
 function App() {
+  const [{currentOperand , prevOperand, operation},dispatch]=useReducer(reducer,{})
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand">123456</div>
-        <div className="current-opernad">567800</div>
+        <div className="previous-operand">{prevOperand} {operation}</div>
+        <div className="current-opernad">{currentOperand}</div>
       </div>
         <button className="span-two">AC</button>
         <button>DEL</button>
